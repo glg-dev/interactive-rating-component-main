01c8586d7d8f43d48d2e063b23b08e754f931d4b
Gaël <gael77@hotmail.fr>
2022-06-05 14:19:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const rating = document.querySelectorAll('.container__rating--item');
+const submitButton = document.querySelector('.container__button--submit');
+const thanks = document.querySelector('.thanks');
+const result = document.getElementById('result');
+
+let ratingValue = 0;
+
+rating.forEach(element => {
+    element.addEventListener('click', () => rate(element))
+    element.removeEventListener('click', () => rate(element))
+})
+
+submitButton.addEventListener('click', submitForm);
+
+function rate(e) {
+  rating.forEach(item => {
+    item.classList.remove('container__rating--active');
+  })
+  e.classList.add('container__rating--active');
+  ratingValue = e.firstElementChild.value;
+}
+
+function submitForm() {
+  container.style.display = 'none';
+  thanks.style.display = 'flex';
+  result.innerHTML = ratingValue;
+}
